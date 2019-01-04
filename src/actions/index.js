1f4bbc02722bf19d30b0b86105b481258b32fa72
sceptreCnsn <cansin.memis@clb.lcl>
2019-01-04 18:29:04 +0300

    commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
 import jsonPlaceholder from "../apis/jsonplaceholder";
 
-export const fetchPosts = async () => {
-  const response = await jsonPlaceholder.get("/posts");
-  return {
-    type: "FETCH_POSTS",
-    payload: response
-  };
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    dispatch({type:'FETCH_POSTS', payload: response.data});
 };
+
+export const fetchUser =  (id) => async dispatch => { 
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    console.log(response);
+    dispatch({type:'FETCH_USER', payload:response.data});
+}
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,17 +1,39 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+
+  renderList() {
+    return this.props.posts.map(p => {
+      return (
+        <div className="item" key={p.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h3>{p.title}</h3>
+              <p>{p.body}</p>
+            </div>
+          </div>
+          <UserHeader userId={p}/>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>PostList</div>;
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostList, { fetchPosts });
